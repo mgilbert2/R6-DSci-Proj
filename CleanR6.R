f2f260ac8bac0e89f8bc9a4930906593209495cb
mgilbert2 <mgilbert2@ucmerced.edu>
2023-11-02 13:32:24 -0700

    Additional Scourcing
    Added the column for pick and ban rates and starting into the recoil statistics.

--- CleanR6.R
+++ CleanR6.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(stringr)
-library(dplyr)
+
  View(my_table)
  colnames(my_table)
 # [1] "Name"                 "Type"                 "Operator"            
@@ -8,12 +8,12 @@ library(dplyr)
 # [7] "ROF"                  "Magazine"             "Suppressor"          
 # [10] "ACOG"                 ""                    
 colnames(my_table)[11] <- "Range"
-
-df<-my_table
-#   
-# df <- df %>%
-#   mutate(Operators = str_split(Operator, "(?=[A-Z])")) %>%
-#   unnest(Operators)
+# 
+# df<-my_table
+# #   
+# # df <- df %>%
+# #   mutate(Operators = str_split(Operator, "(?=[A-Z])")) %>%
+# #   unnest(Operators)
 
 library(dplyr)
 library(tidyr)
@@ -30,3 +30,36 @@ my_table_split <- my_table %>%
 # View the result
 print(my_table_split)
 view(my_table_split)
+
+
+high_pick <- c("Finka", "Iana", "Sledge", "Thatcher", "Twitch", "Alibi", "Jager", "Melusi", "Mute")
+average_pick <- c("Valkyrie", "Thunderbird", "Smoke", "Bandit", "Kaid", "Wamai", "Mozzie", "Azami", "Lesion", "Kapkan", "Aruni", "Oryx", "Jackal", "Ace", "Thermite", "Zofia", "Maverick", "Hibana", "Ash", "Nomad")
+low_pick <- c("Buck", "Osa", "Nokk", "Flores", "Amaru", "IQ", "Zero", "Montagne", "Dokkaebi", "Blackbeard", "Lion", "Blitz", "Kali", "Capitao", "Fuze", "Ying", "Gridlock", "Glaz", "Sens", "Vigil", "Doc", "Ela", "Mira", "Goyo", "Frost", "Thorn", "Maestro", "Pulse", "Rook", "Castle", "Clash", "Warden", "Caveira", "Echo", "Tachanka")
+
+my_table_long <- my_table_split %>%
+  mutate(PickRate = case_when(
+    Operator %in% high_pick ~ "High Pick Rate",
+    Operator %in% average_pick ~ "Average Pick Rate",
+    Operator %in% low_pick ~ "Low Pick Rate",
+    TRUE ~ NA_character_
+  ))
+
+# View the modified table with the new PickRate column
+glimpse(my_table_long)
+
+my_table_long <- my_table_long %>%
+  rename(Weapon = Name)
+
+library(ggplot2)
+
+# Filter for High Pick Rate weapons
+high_pick_rate_weapons <- my_table_long %>% filter(`PickRate` == 'High Pick Rate')
+
+# Create the ggplot
+ggplot(high_pick_rate_weapons, aes(x = Weapon)) +
+  geom_bar() +
+  coord_flip() +  # Flipping the axis for better visualization
+  labs(title = "Weapons associated with 'High Pick Rate' Operators", 
+       x = "Weapons", 
+       y = "Count") +
+  theme_minimal()
